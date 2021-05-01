fadcce45f1a5c2031886c4441ce198a5dcc2fd73
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-05-01 17:57:18 -0400

    Add Post Signup to App Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 require 'rack-flash'
 require 'securerandom'
 
+
 class ApplicationController < Sinatra::Base
   enable :sessions
   set :session_secret, ENV.fetch('SESSION_SECRET'){SecureRandom.hex(64)}
-  
   use Rack::Flash
 
   configure do
@@ -25,22 +25,39 @@ class ApplicationController < Sinatra::Base
       flash[:message] = "Incorrect Username or Password. Please try again"
      redirect to '/' 
     end
- end
-
-  get '/signup' do 
-    erb :signup
   end
 
-  # post '/signup' do
-  #   user = User.new(username: params[:username], password: params[:password])
-  #   if user.save
-  #     session[:user_id] = user.id
-  #     redirect '/users'
-  #   else
-  #     flash[:message] = "ENTER A USERNAME & PASSWORD TO CONTINUE"
-  #     redirect "/signup"
-  #   end
-  # end
+  get '/signup' do 
+    if !!session[:user_id]
+      @user = Helpers.current_user(session)
+      redirect "/users/#{@user.slug}"
+    else
+      erb :signup
+    end
+  end
+  
+
+  post '/signup' do
+    user = User.find_by(username: params[:username])
+    if user 
+      flash[:message] = "Username already taken, select different Username."
+      redirect "/signup"
+    else
+      user = User.new(username: params[:username], password: params[:password])
+      if user.save
+        session[:user_id] = user.id
+        redirect '/users/new'
+      else
+        flash[:message] = "ENTER A USERNAME & PASSWORD TO CONTINUE"
+        redirect "/signup"
+      end
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/'
+  end
 
 
 end
