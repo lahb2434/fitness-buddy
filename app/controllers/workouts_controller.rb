8fdf96a011a024b1d20cd090368be69d77822116
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-05-04 09:35:45 -0400

    Update error flash and validation logic

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,72 +1,122 @@
 class WorkoutsController < ApplicationController
 
-  # GET: /workouts
-  get "/workouts" do
-    erb :"/workouts/index.html"
-  end
-
+  
   # GET: /workouts/new
   get "/workouts/new" do
-    @exercises = Exercise.all
-    @muscles = Muscle.all
-    erb :"/workouts/new.html"
+    if logged_in?
+      @exercises = Exercise.all
+      @muscles = Muscle.all
+      erb :"/workouts/new.html"
+    else
+      flash[:message] = "Something went wrong, please login."
+      redirect '/'
+    end
   end
 
   # POST: /workouts
   post "/workouts" do
-    routine = Routine.find(session[:routine_id])
-    workout = Workout.new(params[:workout])
-    workout.routine = routine
-    workout.save
-    if params[:exercise][:name].empty?
-      exercise = Exercise.find(params[:exercise_id])
+    
+    if session[:routine_id] 
+      routine = Routine.find(session[:routine_id])
+      if !params[:workout].values.any?("")
+        workout = Workout.new(params[:workout])
+        workout.routine = routine
+          if params[:exercise][:name].empty? && params[:muscle_id].empty?
+            if !params[:exercise_id].empty?
+              exercise = Exercise.find(params[:exercise_id])
+            else
+              flash[:message] = "Please fill in all areas to continue."
+              redirect '/workouts/new'
+            end
+          else
+            muscle = Muscle.find(params[:muscle_id])
+            exercise = Exercise.new(params[:exercise])
+            exercise.muscle = muscle
+          end
+      else
+        flash[:message] = "Please fill in all areas to continue."
+        redirect '/workouts/new'
+      end
+        workout.save
+        exercise.exercise_workouts.build(workout: workout)
+        exercise.save
+        binding.pry
+        redirect "/routines/#{routine.id}"
     else
-      muscle = Muscle.find(params[:muscle_id])
-      exercise = Exercise.new(params[:exercise])
-      exercise.muscle = muscle
-    end
-    exercise.exercise_workouts.build(workout: workout)
-    exercise.save
-    redirect "/routines/#{routine.id}"
+      flash[:message] = "Something went wrong."
+      redirect "/programs/#{current_user.id}"
+    end    
   end
 
   # GET: /workouts/5
   get "/workouts/:id" do
-    erb :"/workouts/show.html"
+    if logged_in?
+      erb :"/workouts/show.html"
+    else
+      flash[:message] = "Something went wrong, please login."
+      redirect '/'
+    end
   end
 
   # GET: /workouts/5/edit
   get "/workouts/:id/edit" do
-    @workout = Workout.find(params[:id])
-    @exercises = Exercise.all
-    erb :"/workouts/edit.html"
+    if logged_in?
+      routine = Routine.find(session[:routine_id])
+      @workout = Workout.find(params[:id])
+      if @workout.routine.program.user == current_user
+        @exercises = Exercise.all
+        @muscles = Muscle.all
+        erb :"/workouts/edit.html"
+      else
+        flash[:message] = "Something went wrong."
+        redirect "/routine/#{routine.id}"
+      end
+    else
+      flash[:message] = "Something went wrong, please login."
+      redirect '/'
+    end
   end
 
   # PATCH: /workouts/5
   patch "/workouts/:id" do
     routine = Routine.find(session[:routine_id])
     workout = Workout.find(params[:id])
-    workout.update(params[:workout])
-    
-    if params[:exercise][:name].empty?
-      workout.exercise_ids = [params[:exercise_id]]
-    else  
-      muscle = Muscle.find(params[:muscle_id])
-      exercise = Exercise.new(params[:exercise])
-      exercise.muscle = muscle
-      exercise.save
-      workout.exercise_ids = [exercise.id]
-      exercise.save
+    if !params[:workout].values.any?("")
+      if params[:exercise][:name].empty? && params[:muscle_id].empty?
+        if !params[:exercise_id].empty?
+          workout.exercise_ids = [params[:exercise_id]]
+        else
+          flash[:message] = "Please fill in all areas to continue."
+          redirect "/workouts/#{workout.id}/edit"
+        end 
+      else  
+        muscle = Muscle.find(params[:muscle_id])
+        exercise = Exercise.new(params[:exercise])
+        exercise.muscle = muscle
+        exercise.save
+        workout.exercise_ids = [exercise.id]
+        exercise.save
+      end
+      workout.update(params[:workout])
+      workout.save
+      redirect "/routines/#{routine.id}"
+    else
+      flash[:message] = "Please fill in all areas to continue."
+      redirect "/workouts/#{workout.id}/edit"
     end
-    workout.save
-    redirect "/routines/#{routine.id}"
   end
 
   # DELETE: /workouts/5/delete
   delete "/workouts/:id/delete" do
     routine = Routine.find(session[:routine_id])
     workout = Workout.find(params[:id])
-    workout.delete
-    redirect "/routines/#{routine.id}"
+    if workout.routine.program.user == current_user
+      workout.delete
+      redirect "/routines/#{routine.id}"
+    else
+      flash[:message] = "Something went wrong."
+      redirect "/workouts/#{workout.id}"
+    end
+
   end
 end
